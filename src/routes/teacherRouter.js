fe650127f9caff7bfe20947a629dee5212801ddc
Immanuel Victor <immanuelvictor.a.f@gmail.com>
2023-01-29 17:36:37 -0300

    feat: adiciona middleware de autenticação e protecao de rotas

--- src/routes/teacherRouter.js
+++ src/routes/teacherRouter.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createTeacher, getAllTeachers, getTeacher, updateTeacher, deleteTeacher } from '../controllers/teacherController.js';
+import { checkAuth } from "../middleware/requireAuth.js"
 
 const router = express.Router();
 
@@ -7,6 +8,7 @@ router.get('/:registration', getTeacher) // retorna um professor
 router.get('/', getAllTeachers) // retorna todos os professores
 
 
+router.use(checkAuth)
 router.post('/', createTeacher) // cria um professor
 router.put('/:registration', updateTeacher) // altera info de professor
 router.delete('/:registration', deleteTeacher) // deleta professor
